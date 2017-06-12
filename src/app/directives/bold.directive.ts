5959dc3b912a0bf5176bbcc7ed911627ad8315f8
imatiash <ihor.matiash@gmail.com>
2017-06-12 19:49:18 +0300

    Directives: HostListener
    Directives: HostListener

--- src/app/directives/bold.directive.ts
+++ src/app/directives/bold.directive.ts
@@ -1,6 +1,11 @@
-import{Directive, ElementRef,Input, Renderer} from '@angular/core';
+import{Directive, ElementRef, /*HostListener, */ Input, Renderer, Renderer2} from '@angular/core';
+
 @Directive({
-    selector:'[bold]'
+    selector:'[bold]',
+    host:{
+         '(mouseenter)':'onMouseEnter()',
+         '(mouseleave)':'onMouseLeave()' 
+          }
 })
 
 export class boldDirective{
@@ -8,8 +13,34 @@ export class boldDirective{
 
 constructor(
     private elRef: ElementRef,
-    private renderer: Renderer)
+    private renderer: Renderer2)
+    {
+        // this.renderer.setElementStyle(this.elRef.nativeElement, 'font-weight', 'bold');
+        this.renderer.setStyle(this.elRef.nativeElement, 'cursor', 'pointer');
+    }
+//I
+// @HostListener('mouseenter') onMouseEnter()
+// {
+//     this.setFontWeight('bold')
+// }
+
+// @HostListener('mouseleave') onMouseLeave()
+// {
+//     this.setFontWeight('normal')
+// }
+
+//II
+onMouseEnter()
 {
-    this.renderer.setElementStyle(this.elRef.nativeElement, 'font-weight', 'bold')
+    this.setFontWeight('bold')
 }
+
+onMouseLeave()
+{
+    this.setFontWeight('normal')
+}
+
+private setFontWeight(val: string) {
+        this.renderer.setStyle(this.elRef.nativeElement, 'font-weight', val);
+    }
 }
